0d88ec7a034e1e728046f11619cbf9e05f42cb92
Harendra Singh <harendrasingh2205@gmail.com>
2018-04-22 12:32:42 +0530

    frontend

---
+++ mypig game/myscript.js
@@ -0,0 +1,6 @@
+//document.querySelector('#player-2 h1').textContent ='hello';
+//console.log(document.querySelector('#player-2 h1').textContent);
+//console.log('hello');
+//console.log(document.querySelector('#score2').textContent);
+//document.querySelector('#score2').textContent=0;
+//console.log(document.querySelector('#score2').textContent);
